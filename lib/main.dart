71db70385e3ad28260fce578a7bfa10aeb09514f
Gerhard Herbert <gerhard.herbert@avira.com>
2018-04-20 17:18:41 +0200

    added some ty-catch for authentication.

--- lib/main.dart
+++ lib/main.dart
@@ -18,23 +18,25 @@ final auth = FirebaseAuth.instance;
 final usageStats = new UsageStats();
 final usage = new Usage.Usage(usageStats);
 
-
 Future<bool> silentLogIn() async {
- GoogleSignInAccount user = googleSignIn.currentUser;
-  if (user == null)
-    user = await googleSignIn.signInSilently();
-  
-  if (user == null)
+  GoogleSignInAccount user = googleSignIn.currentUser;
+  if (user == null) user = await googleSignIn.signInSilently();
+
+  if (user == null) return false;
+
+  try {
+    await authenticate();
+  } catch (e) {
+    print(e.toString());
     return false;
-  
-  await authenticate();
+  }
   return true;
 }
 
 Future<Null> authenticate() async {
-    if (await auth.currentUser() == null) {
+  if (await auth.currentUser() == null) {
     GoogleSignInAuthentication credentials =
-    await googleSignIn.currentUser.authentication;
+        await googleSignIn.currentUser.authentication;
     await auth.signInWithGoogle(
       idToken: credentials.idToken,
       accessToken: credentials.accessToken,
@@ -43,32 +45,34 @@ Future<Null> authenticate() async {
 }
 
 Future<Null> saveUsage() async {
-    if (UsageStore.lastWriteTime != null && UsageStore.lastWriteTime.millisecondsSinceEpoch >= Usage.Usage.getStartOfDay(-1).millisecondsSinceEpoch) {
-      print("Usage already stored.");
-      return;
-    }
+  if (UsageStore.lastWriteTime != null &&
+      UsageStore.lastWriteTime.millisecondsSinceEpoch >=
+          Usage.Usage.getStartOfDay(-1).millisecondsSinceEpoch) {
+    print("Usage already stored.");
+    return;
+  }
 
-    var usageStatsYesterday = await usage.getUsageStats(-1);
-    usageStatsYesterday.sort((a, b) => b.duration.compareTo(a.duration));
-    var timeYesterday = Usage.Usage.calcDuration(usageStatsYesterday);
-    UsageStore.store(Usage.Usage.getStartOfDay(-1), timeYesterday, usageStatsYesterday);
-    print("Usage was successfully stored.");
+  var usageStatsYesterday = await usage.getUsageStats(-1);
+  usageStatsYesterday.sort((a, b) => b.duration.compareTo(a.duration));
+  var timeYesterday = Usage.Usage.calcDuration(usageStatsYesterday);
+  UsageStore.store(
+      Usage.Usage.getStartOfDay(-1), timeYesterday, usageStatsYesterday);
+  print("Usage was successfully stored.");
 }
 
 var globalLoggedIn = false;
-Future<Null>   main() async {
-    final int alarmID = 99;
-    silentLogIn().then((u) async {
-      globalLoggedIn = u;
-      runApp(new TimieApp());
-      var result = await AndroidAlarmManager.periodic(const Duration(minutes: 30), alarmID, saveUsage);
-      print ('$result');
-    });
+Future<Null> main() async {
+  final int alarmID = 99;
+  silentLogIn().then((u) async {
+    globalLoggedIn = u;
+    runApp(new TimieApp());
+    var result = await AndroidAlarmManager.periodic(
+        const Duration(minutes: 30), alarmID, saveUsage);
+    print('$result');
+  });
 }
 
-class TimieApp extends StatelessWidget 
-{  
-
+class TimieApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -91,42 +95,45 @@ class TimieApp extends StatelessWidget
 class HomeState extends State<Home> {
   var loggedIn = globalLoggedIn;
 
-  Future<Null> logIn() async
-  {
+  Future<Null> logIn() async {
     GoogleSignInAccount user = googleSignIn.currentUser;
-    if (user != null)
-      return;
+    if (user != null) return;
 
-    user = await googleSignIn.signIn();
-    await analytics.logLogin();
-    await authenticate();
-    setState(() {
-      this.loggedIn = user != null;
-    });
- }
+    try {
+      user = await googleSignIn.signIn();
+      await analytics.logLogin();
+      await authenticate();
+      setState(() {
+        this.loggedIn = user != null;
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     if (this.loggedIn)
-      return new TimieHomePage.TimieHomePage(title: "Timie",);
+      return new TimieHomePage.TimieHomePage(
+        title: "Timie",
+      );
 
     return new Card(
-      child: new Padding(
-       padding:   new EdgeInsets.fromLTRB(10.0, 50.0, 10.0, 20.0),
-      child : new Column(
-      children: <Widget>[
-        new FlatButton(
-          child: new Text("Bitte anmelden"),
-           color: Colors.amber,
-           onPressed: logIn,
-          )
-      ],
-    )));
+        child: new Padding(
+            padding: new EdgeInsets.fromLTRB(10.0, 50.0, 10.0, 20.0),
+            child: new Column(
+              children: <Widget>[
+                new FlatButton(
+                  child: new Text("Bitte anmelden"),
+                  color: Colors.amber,
+                  onPressed: logIn,
+                )
+              ],
+            )));
   }
 }
 
-class Home extends StatefulWidget
-{
+class Home extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new HomeState();
 }
